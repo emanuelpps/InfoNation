4b18e99fbea3cf4cff7d1f542392d82d80d5860c
emanuelpps <emanuelpages.ps@gmail.com>
2024-09-26 21:12:03 -0300

    feat: added search bar

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,9 +1,23 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
 import React from "react";
 
 type Props = {};
 
-function SearchBar({}: Props) {
-  return <div>SearchBar</div>;
-}
+const SearchBar = (props: Props) => {
+  return (
+    <View>
+      <TextInput placeholder="Search..." />
+    </View>
+  );
+};
 
 export default SearchBar;
+
+const styles = StyleSheet.create({
+  inputStyles: {
+    height: 40,
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+});
